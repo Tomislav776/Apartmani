bcb40c192409299cd83db2207a56fca307d20fb4
Tomislav <zdencec52@gmail.com>
2017-04-03 14:37:44 +0200

    Middle, admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
 
     public function __construct()
     {
-        $this->middleware('Role');
+        $this->middleware('Admin');
     }
 
 
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'Role' => \App\Http\Middleware\RoleMiddleware::class,
+        'Admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
